c56e6b5bfb72e508f924bb91f94698ca30337d1e
AmirAli Pourmohammadi <amiralipourmohammdi290@gmail.com>
2025-12-10 11:07:44 +0330

    first commit

---
+++ Amir/Footer.php
@@ -0,0 +1,4 @@
+<div style="background-color: black; color: aliceblue; height: 100px;">
+    <h6>Amirali pourmohammadi</h6>
+    <p>created by ERASTO ❤️</p>
+</div>
---
+++ Amir/Topbar.php
@@ -0,0 +1,6 @@
+<div style="background-color: antiquewhite;">
+    <h1>به سایت امیرعلی خوش آمدید</h1>
+    <a href="login.php">login</a>
+    <a href="register.php">register</a>
+    <a href="/">exit</a>
+</div>
