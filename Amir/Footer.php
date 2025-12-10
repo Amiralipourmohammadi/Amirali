f92468f9c9259e58b0cdd4466c0658df1453e8cb
AmirAli Pourmohammadi <amiralipourmohammdi290@gmail.com>
2025-12-10 11:23:31 +0330

    Update Footer.php

--- Amir/Footer.php
+++ Amir/Footer.php
@@ -2,3 +2,7 @@
     <h6>Amirali pourmohammadi</h6>
     <p>created by ERASTO ❤️</p>
 </div>
+<div style="background-color: black; color: aliceblue; height: 100px;">
+    <h6>Amirali pourmohammadi</h6>
+    <p>created by ERASTO ❤️</p>
+</div>
